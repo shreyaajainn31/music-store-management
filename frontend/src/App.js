718e68a53dc64b81fff79f62bbefd32e5512d6db
shreyajain <shreyajain981@gmail.com>
2023-02-02 20:09:37 -0800

    created components to redirect to another page from login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import LoginForm from './components/Login/LoginForm';
+import Register from './components/Register/Register';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-     <LoginForm/>
+       <Router>
+      <Routes>
+        <Route path="/" exact component={LoginForm} />
+        <Route path="/register" component={Register} />
+        <Route path="/home" component={Home} />
+      </Routes>
+    </Router>
     </div>
   );
 }
--- frontend/src/components/Login/LoginForm.js
+++ frontend/src/components/Login/LoginForm.js
@@ -1,39 +1,65 @@
-import React, {useState} from "react";
-import './LoginForm.css';
-function LoginForm(){
+import React, { useState, useEffect } from "react";
+import "./LoginForm.css";
+import Home from "../Home/Home";
+import Register from "../Register/Register";
+import { Route, Link, Switch, Location, useNavigate } from "react-router-dom";
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const handleShowPassword = e => {
-        setShowPassword(e.target.checked);
-    }
+function LoginForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useLocation();
+  const handleShowPassword = (e) => {
+    setShowPassword(e.target.checked);
+  };
+  const [inputButton, setInputButton] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+  const setInputButtonSubmit = (e) => {
+    e.preventDefault();
+    setInputButton("submit");
+    history.push("/home");
+  };
+  const setInputButtonRegister = (e) => {
+    e.preventDefault();
+    setInputButton("register");
+    history.push("/register");
+  };
 
-    return (
-
-        <div className = 'sign-up'>
-            <h2>Login</h2>
-           <form>
-            <label for = "username"> Email: </label>
-            <input className = 'sign' type = "email" id = "username" placeholder="Enter Email"/>
-            <br></br>
-            <label for = "pass"> Password: </label>
-            <input className = 'sign' type = {showPassword ? "text" : "password"} id = "pass" placeholder="Enter Password"/> <input type = "checkbox" onChange={handleShowPassword}/> <label>Show Password?</label>
-            <br></br>
-            <button className = 'sign' type = "submit"> Submit </button>
-            
-            <button className = 'sign'>New User</button>
-            </form> 
-        </div>
-
-    );
-
+  return (
+    <div className="sign-up">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username"> Email: </label>
+        <input
+          className="sign"
+          type="email"
+          id="username"
+          placeholder="Enter Email"
+        />
+        <br />
+        <label htmlFor="pass"> Password: </label>
+        <input
+          className="sign"
+          type={showPassword ? "text" : "password"}
+          id="pass"
+          placeholder="Enter Password"
+        />{" "}
+        <input type="checkbox" onChange={handleShowPassword} />{" "}
+        <label>Show Password?</label>
+        <br />
+        <button className="sign" type="submit" onClick={setInputButtonSubmit}>
+          Submit
+        </button>
+        <button className="sign" onClick={setInputButtonRegister}>
+          New User
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
-
-
-
-
-
-
