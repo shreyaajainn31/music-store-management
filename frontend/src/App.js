d5d3548af943801bd869fe29744d98aa4091b3a0
shreyajain <shreyajain981@gmail.com>
2023-02-02 22:18:58 -0800

    shifted navbar to overall directory of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import LoginForm from "./components/Login/LoginForm";
 import Home from "./components/Home/Home";
 import Register from "./components/Register/Register";
-import About from "./components/Home/Navbar/Pages/About";
-import Contact from "./components/Home/Navbar/Pages/ContactUs";
-import YourMusic from "./components/Home/Navbar/Pages/YourMusic";
-import Music from "./components/Home/Navbar/Pages/Music";
+import About from "./components/Navbar/Pages/About";
+import Contact from "./components/Navbar/Pages/ContactUs";
+import YourMusic from "./components/Navbar/Pages/YourMusic";
+import Music from "./components/Navbar/Pages/Music";
 
 function App() {
   return (
